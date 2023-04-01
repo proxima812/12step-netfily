96071737bf7f1a7cedae37a095943460bb72f885
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-04-01 17:16:25 +0600

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+ site: "https://12.netlify.app",
+ integrations: [
+  sitemap(),
+  robotsTxt(),
+  tailwind(),
+  compress({
+   css: true,
+   html: true,
+   img: true,
+   js: true,
+   svg: true,
+  }),
+ ],
+});

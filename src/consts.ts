9bbf667bced688575cfff33f4d434c85087b6950
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-04-01 19:43:21 +0600

    metrika

--- src/consts.ts
+++ src/consts.ts
@@ -5,7 +5,7 @@ export const TITLE_SITE = "Материалы об 12 шагах";
 export const WEB_APP_TITLE = "Материалы";
 
 export const VERIFICATION_GOOGLE = "SeOherPqDJhKKBAXgON3Y0Vl22B4kKEaKP_jz8lE_mM";
-export const VERIFICATION_YANDEX = "1f9051c09d839850";
+export const VERIFICATION_YANDEX = "db1dc2c0bd419639";
 
 export const EMAIL_CONTACT = "sam-gold-8@ya.ru";
 export const TELEGRAM_CONTACT = "https://t.me/AA_bill";

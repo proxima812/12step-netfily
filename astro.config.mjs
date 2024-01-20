51fcfcc6226127686dff0ec59a6ab4f4cda22894
proxima812 <mrzuc2015@yandex.ru>
2024-01-20 11:07:53 +0600

    /aa/dzho-i-charli: https://12steps.vercel.app/bigbook/seminar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,39 @@
-import prefetch from "@astrojs/prefetch";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
-import robotsTxt from "astro-robots-txt";
-import { defineConfig } from "astro/config";
-import serviceWorker from "astrojs-service-worker";
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import compress from "astro-compress"
+import robotsTxt from "astro-robots-txt"
+import { defineConfig } from "astro/config"
+import serviceWorker from "astrojs-service-worker"
 
-import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
- site: "https://12step.netlify.app",
- integrations: [
-  serviceWorker({
-   autoRegister: true,
-  }),
-  sitemap(),
-  robotsTxt(),
-  tailwind(),
-  compress({
-   css: true,
-   html: true,
-   img: true,
-   js: true,
-   svg: true,
-  }),
-  prefetch(),
-  react(),
-  partytown({
-   config: {
-    forward: ["dataLayer.push"],
-   },
-  }),
- ],
-});
+	site: "https://12step.netlify.app",
+	redirects: {
+		"/aa/dzho-i-charli": "https://12steps.vercel.app/bigbook/seminar",
+	},
+	integrations: [
+		serviceWorker({
+			autoRegister: true,
+		}),
+
+		sitemap(),
+		robotsTxt(),
+		tailwind(),
+		compress({
+			css: true,
+			html: true,
+			img: true,
+			js: true,
+			svg: true,
+		}),
+		react(),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+	],
+})
